b70c1b1e4b07bb8b23f0e8b2403ef48485dec0a8
Quyen Pham <hoangquyen174@gmail.com>
2022-02-11 15:11:50 -0600

    upload eight-ball

---
+++ React-eightball/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EightBall from "./EightBall";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EightBall />
+    </div>
+  );
+}
+
+export default App;
