f64a855a028c2c9c7460c8db366629db3b5a6359
Quyen Pham <hoangquyen174@gmail.com>
2022-02-11 15:46:12 -0600

    upload color-boxes

---
+++ React-colorBoxes/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+import BoxesContainer from './BoxesContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <BoxesContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ React-colorBoxes/src/Box.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Box.css";
+
+const Box = (props) => {
+    return (
+        <div className="Box" style={{ backgroundColor: props.color }}>
+            
+        </div>
+    )
+}
+
+export default Box;
