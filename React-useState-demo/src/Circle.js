14cd887bb802ee584f9a36548b2643209e962706
Quyen Pham <hoangquyen174@gmail.com>
2022-02-11 22:55:13 -0600

    add circles position

--- React-useState-demo/src/Circle.js
+++ React-useState-demo/src/Circle.js
@@ -1,13 +1,18 @@
 import React from "react";
 import './Circle.css';
 
-const Circle = (props) => {
+const Circle = ({color, x, y, idx, changePosition}) => {
     return (
         <div 
+            onClick={() => changePosition(idx)}
             className="Circle"
-            style={{ backgroundColor: props.color }}
+            style={{ backgroundColor: color,
+                     position: 'absolute',
+                     top: `${y}vh`,
+                     left: `${x}vw`   
+                    }}
         >
-            {props.idx + 1} 
+            {idx + 1} 
         </div>
     )
 }
--- React-useState-demo/src/ColorCircle.js
+++ React-useState-demo/src/ColorCircle.js
@@ -2,8 +2,12 @@ import React, {useState} from "react";
 import Circle from "./Circle";
 import ColorButton from "./ColorButton";
 
+function getRandom(min = 0, max = 100) {
+    return Math.random() * (max - min) + min;
+}
+
 const ColorCircle = () => {
-    const [circles, setCircles] = useState(['cornflowerblue', 'peachpuff', 'lavender']) 
+    const [circles, setCircles] = useState([]) 
     const addCircle = (color) => {
         // setCircles(circles => {
         //     const newCircles = circles.slice();
@@ -11,16 +15,30 @@ const ColorCircle = () => {
         //     return newCircles;
         // })
         setCircles(circles => {
-            return [...circles, color]
+            return [...circles, {color, x: getRandom(), y: getRandom()}]
         })
+    }
+
+    const changePosition = idx => {
+        setCircles(circles => {
+            const copy = [...circles];
+            copy[idx].x = getRandom();
+            copy[idx].y = getRandom();
+            return copy;
+        }) 
     }
 
     return (
         <div>
             <ColorButton addCircle={addCircle} options={['peachpuff', 'lightsteelblue', 'paleturquoise']} />
             {/* <ColorButton addCircle={addCircle} options={['orange','teal','magenta']} /> */}
-            {circles.map((color, idx) => (
-                <Circle color={color} idx={idx} />
+            {circles.map(({color, x, y}, idx) => (
+                <Circle changePosition={changePosition}
+                        color={color} 
+                        idx={idx}  
+                        key={idx} 
+                        x={x} 
+                        y={y} />
             ))}
         </div>
     )
