5697a904d1b889ed28a5677f4e0cc01130c9191b
Quyen Pham <hoangquyen174@gmail.com>
2022-02-15 21:36:11 -0600

    upload form-demo

---
+++ React-form-demo/jobly.sql
@@ -0,0 +1,23 @@
+
+-- Create a database (with a small amount of starter data) and a test database.
+
+
+
+\echo 'Delete and recreate jobly db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE jobly;
+CREATE DATABASE jobly;
+\connect jobly
+
+\i jobly-schema.sql
+\i jobly-seed.sql
+
+\echo 'Delete and recreate jobly_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE jobly_test;
+CREATE DATABASE jobly_test;
+\connect jobly_test
+
+\i jobly-schema.sql
