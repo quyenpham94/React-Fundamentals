19c49c24aa418d1abb2b2d0ebcd1fd07d1b6af84
Quyen Pham <hoangquyen174@gmail.com>
2022-02-21 11:13:00 -0600

    upload toggle state

--- React-custom-hooks/src/App.js
+++ React-custom-hooks/src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import MoodClicker from './MoodClicker';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-   
+      <MoodClicker />
     </div>
   );
 }
